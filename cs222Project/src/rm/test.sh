2de2e00982341904826176d83559c0e21488216b
Saumitra Shukla <saumitra90@gmail.com>
2017-10-31 09:20:50 -0700

    tesh.sh for rm

---
+++ cs222Project/src/rm/test.sh
@@ -0,0 +1,52 @@
+rm Tables
+
+rm Columns
+
+rm tbl_employee*
+
+rm tbl_b*
+
+
+make clean
+
+make
+
+./rmtest_create_tables
+
+./rmtest_00
+
+./rmtest_01
+
+./rmtest_02
+
+./rmtest_03
+
+./rmtest_04
+
+./rmtest_05
+
+./rmtest_06
+
+./rmtest_07
+
+./rmtest_08
+
+./rmtest_09
+
+./rmtest_10
+
+./rmtest_11
+
+./rmtest_12
+
+./rmtest_13
+
+./rmtest_13b
+
+./rmtest_14
+
+./rmtest_15
+
+./rmtest_extra_1
+
+./rmtest_extra_2
